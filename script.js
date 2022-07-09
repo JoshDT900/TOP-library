bc8eb07f98d1f5dfb223ae1178fe01087ace8643
JoshDT900 <DJTroutman900@gmail.com>
2022-07-09 15:43:18 -0400

    Add functionality to the read button

--- script.js
+++ script.js
@@ -51,12 +51,15 @@ myLibrary.forEach(myBook => {
   for (const [key, value] of Object.entries(myBook)) {
     const newData = document.createElement("td")
     const newBtn = document.createElement("button")
+    const divTxt = document.createElement("div")
     newBtn.classList.add("read_button")
 
     console.log(myBook.book.replace(/[^\w]/g, "_"));
 
     if (key === 'read') {
-      newData.textContent = value;
+      divTxt.textContent = value;
+
+      newData.append(divTxt)
       newData.classList.add(myBook.book.replace(/[^\w]/g, "_").toLowerCase())
       
       newBtn.textContent = "Read?"  
@@ -71,9 +74,14 @@ myLibrary.forEach(myBook => {
   tableBox.appendChild(newRow);
 });
 
-function readSwitch() {
+function readSwitch(e) {
   myLibrary.forEach(myBook => {
-    if ()
-  });
+    if (myBook.book.replace(/[^\w]/g, "_").toLowerCase() === e.target.parentElement.classList.value) {
+      myBook.read = !myBook.read
+      e.target.previousSibling.textContent = myBook.read
+    }
+  })
 }
 
+const resetBtn = document.querySelectorAll('.read_button')
+resetBtn.forEach(btn => btn.addEventListener('click', readSwitch));
